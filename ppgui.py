25bf3ad3597eca6e730873aa5c45ac1576f045da
cjlester <cjlester@outlook.com>
2024-06-26 21:35:52 -0700

    pi3b

--- ppgui.py
+++ ppgui.py
@@ -2,20 +2,19 @@ import os
 import logging
 import xml.etree.ElementTree as ET
 import pypdfium2 as pdfium
-import epdemulator 
-from PIL import Image, ImageFont
+from IT8951 import constants
+from IT8951.display import AutoEPDDisplay
+from PIL import Image, ImageFont, ImageDraw
 from getkey import getkey, keys
-import sys
-sys.path.insert(1, '/usr/local/bin')
-import paperpi
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
-epd = epdemulator.EPD(config_file="epd10in3", use_tkinter=False, use_color=False, update_interval=1, reverse_orientation=False)
-epd.init()    
-width, height = epd.width, epd.height     
+display = AutoEPDDisplay(vcom=-1.71, spi_hz=24000000, rotate='CW')
+# display.epd.wait_display_ready() # so that we're not timing the previous operations
+
+width, height = display.width, display.height     
 font = ImageFont.truetype(os.path.join(current_dir, 'Arial.ttf'), 48)
 
 try:
@@ -39,78 +38,69 @@ try:
 except:
     print('xml error')
 
+def select_plate():
+        
+    display.clear() 
+    y = 150
+    c = 0
+    i = 0        
 
-class chart():
-   
-    def select_plate():
-            
-        y = 150
-        c = 0
-        epd.Clear(255)
-        draw = epd.draw
+    while True:
+       
+        draw = ImageDraw.Draw(display.frame_buf) 
+        draw.rectangle((50, 50, 999, y + 999), fill=255, outline=255)         
+        draw.text((50, 50), "SELECT APPROACH FOR " + airport_name.get('ID') + ":", font = font)
         i = 0        
+    
+        for chrt in chrts:               
+            draw.text((100, 150 + (i * 50)), chrt, font=font, fill=0)
+            i += 1
 
-        while True:
+        draw.rectangle((98, y, 700, y + 52), fill=0, outline=0)
+        draw.text((100, y), chrts[c], font=font, fill=255) 
+        display.draw_partial(constants.DisplayModes.DU) 
 
-            epd.Clear(255)    ##############
-            draw = epd.draw   ##############
-            draw.text((50, 50),"SELECT APPROACH FOR " + airport_name.get('ID') + ":", font=font, fill=0)
-            i = 0
-        
-            for chrt in chrts:               
-                draw.text((50, 150 + (i * 50)), chrt, font=font, fill=0)
-                i += 1
+        key = getkey()
+        if key == keys.UP:               
+            y -= 50
+            c -= 1
+        if key == keys.DOWN:                
+            y += 50 
+            c += 1   
+        if key == keys.ENTER:
+            select_plate.trgt = pdfs[c]      
+            break      
 
-            draw.rectangle((50, y, 600, y + 52), fill=0, outline=0)
-            draw.text((50, y), chrts[c], font=font, fill=255)        
+def display_plate():    
+    
+    pdf = pdfium.PdfDocument('tppData/' + select_plate.trgt)
+    page = pdf.get_page(0)
+    pil_image = page.render(scale = 300/72).to_pil()
+    image_name =f"plate.bmp"
+    pil_image.save(image_name)  
 
-            image_buffer = epd.get_frame_buffer(draw)
-            epd.display(image_buffer) 
+    image1 = Image.open(os.path.join(current_dir, 'plate.bmp'))
+    new_width1, new_height1 = int(image1.width * 0.875), int(image1.height * 0.875)
+    image1 = image1.resize((new_width1, new_height1))    
+    y_bottom1 = height - new_height1     
+    
+    display.frame_buf.paste(image1, (0, y_bottom1 + 143))
+    display.draw_full(constants.DisplayModes.GC16)
 
-            key = getkey()
-            if key == keys.UP:               
-                y -= 50
-                c -= 1
-            if key == keys.DOWN:                
-                y += 50 
-                c += 1   
-            if key == keys.ENTER:
-                chart.select_plate.trgt = pdfs[c]      
-                break      
-
-    def display_plate():
-
-        pdf = pdfium.PdfDocument('tppData/' + chart.select_plate.trgt)
-        page = pdf.get_page(0)
-        pil_image = page.render(scale = 300/72).to_pil()
-        image_name =f"plate.bmp"
-        pil_image.save(image_name)  
-
-        image1 = Image.open(os.path.join(current_dir, 'plate.bmp'))
-        new_width1, new_height1 = int(image1.width * 0.75), int(image1.height * 0.75)
-        image1 = image1.resize((new_width1, new_height1))
-        x_center1 = (width - new_width1) // 2
-        y_bottom1 = height - new_height1
-        
-        draw = epd.draw
-        epd.Clear(255)
-        epd.paste_image(image1, (x_center1, y_bottom1 - 1))
-        image_buffer = epd.get_frame_buffer(draw)
-        epd.display(image_buffer)
-
-        input('Press enter to go back')
+    y = input('Press enter to go back')
 
 try:
         
     while(True):
 
-        chart.select_plate()
-        chart.display_plate()      
+        select_plate()
+        display_plate()      
 
-except IOError as e:
+except IOError as e:   
     logging.info(e)
 
 except KeyboardInterrupt:
     logging.info("ctrl + c:")
+    display.clear()
     exit()
 
