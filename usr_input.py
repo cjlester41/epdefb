f5b5427a5d0a6e3a189a715691c3474bfd0b136c
cjlester41 <149527318+cjlester41@users.noreply.github.com>
2024-07-01 15:39:56 +0000

    ignore

--- usr_input.py
+++ usr_input.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-import keyboard
+from getkey import getkey, keys
 from time import sleep
 
 class get_gpio():    # get inputs from rotary encoder on pi
@@ -39,29 +39,59 @@ class get_gpio():    # get inputs from rotary encoder on pi
             clkLastState = clkState
             sleep(0.001)
       
-class get_key():    # get keyboard inputs if in emulator mode
-                    ##### must have cursor in termial to capture keyboard inputs #####
+class get_key():    # get keyboard inputs if in emulator mode                   
     
-    def get_input(press):
+    def get_input(press):    # did not work on windows machine. must have cursor in termial to capture keyboard inputs on others
             
-        while press == '':
-        
-            event = keyboard.read_event()
+        key = getkey()
 
-            if event.event_type == keyboard.KEY_DOWN and event.name == 'up':
-                press = 'UP'
-                return press
-            
-            if event.event_type == keyboard.KEY_DOWN and event.name == 'down':
-                press = 'DOWN'
-                return press  
-            
-            if event.event_type == keyboard.KEY_DOWN and event.name == 'enter':
-                press = 'ENTER'
-                return press  
-            
-            if event.event_type == keyboard.KEY_DOWN and event.name == 'esc':
-                 exit()
+        if key == keys.UP:
+            press = 'UP'
+            return press
         
+        if key == keys.DOWN:
+            press = 'DOWN'
+            return press  
+        
+        if key == keys.ENTER:
+            press = 'ENTER'
+            return press  
 
-        
+        # while press == '':   # from keyboard lib (threading prevented keyboardInterrupt...). did not work in codespace
+         
+        #     event = keyboard.read_event()
+
+        #     if event.event_type == keyboard.KEY_DOWN and event.name == 'up':
+        #         press = 'UP'
+        #         return press
+            
+        #     if event.event_type == keyboard.KEY_DOWN and event.name == 'down':
+        #         press = 'DOWN'
+        #         return press  
+            
+        #     if event.event_type == keyboard.KEY_DOWN and event.name == 'enter':
+        #         press = 'ENTER'
+        #         return press  
+            
+        #     if event.event_type == keyboard.KEY_DOWN and event.name == 'esc':
+        #          exit()
+
+        # with keyboard.Events() as events:    # from pynput lib (did not work on codespace. have not tested others)
+
+        #     for event in events:
+
+        #         if event.key == keyboard.Key.up:
+        #             press = 'UP'
+        #             return press
+                
+        #         if event.key == keyboard.Key.down:
+        #             press = 'DOWN'
+        #             return press  
+                
+        #         if event.key == keyboard.Key.enter:
+        #             press = 'ENTER'
+        #             return press  
+                
+        #         if event.key == keyboard.Key.esc:
+        #             exit()
+            
